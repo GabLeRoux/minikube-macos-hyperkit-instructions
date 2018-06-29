b5045582728f6512df8563688f1c29a31f8edcd7
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-06-29 10:33:55 -0400

    Fix typo in minikube name

--- install_minikube_hyperkit.sh
+++ install_minikube_hyperkit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew install docker minkube
+brew install docker minikube
 brew install https://raw.githubusercontent.com/jacobwgillespie/homebrew-core/56ddf54dafcfa0668c2e9491c9183ee5accb1217/Formula/docker-machine-driver-hyperkit.rb
 sudo chown root:wheel /usr/local/bin/docker-machine-driver-hyperkit && sudo chmod u+s /usr/local/bin/docker-machine-driver-hyperkit
 
