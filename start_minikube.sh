e5cc55e725f7cd8834899f7088fec5d4cbdc2ac4
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-07-26 11:11:50 -0400

    Debugging informations

--- start_minikube.sh
+++ start_minikube.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+set -x
+
 sudo touch /var/db/dhcpd_leases
 csrutil status
 
 # https://github.com/kubernetes/minikube/issues/1926
 minikube delete
+
+minikube version
+echo OS: `cat /etc/os-release`
+echo VM driver: `grep DriverName ~/.minikube/machines/minikube/config.json`
+echo ISO version `grep -i ISO ~/.minikube/machines/minikube/config.json`
+
+ls -l .minikube/machines/minikube/hyperkit.pid
 
 # start minikube using hyperkit driver
 minikube start --vm-driver=hyperkit
