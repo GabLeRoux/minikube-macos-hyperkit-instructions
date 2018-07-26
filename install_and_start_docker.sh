a5239587b3365a5c20e59d950f77397061c4180e
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-07-26 11:37:49 -0400

    Thanks internet for --unattended

--- install_and_start_docker.sh
+++ install_and_start_docker.sh
@@ -4,10 +4,13 @@ set -x
 
 brew cask install docker
 
-open --background -a Docker
-docker --version
+sudo /Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
 while ! docker system info > /dev/null 2>&1; do
    sleep 1; 
 done
 
+docker --version
+sudo chmod 777 /var/root/Library /var/root/Library/Containers
+sudo chmod ugo+rw /var/run/docker.sock
+docker ps
