7edebe789edf9021b27f20b0cb0be3a3def931ff
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-07-25 23:32:54 -0400

    Add linking and use sudo to find binary

--- install_minikube_hyperkit.sh
+++ install_minikube_hyperkit.sh
@@ -5,6 +5,11 @@ DRIVER_BINARY_PATH=/usr/local/bin/docker-machine-driver-hyperkit
 
 brew cask install minikube docker
 brew install $FORMULA_URL
+
+# ensure binary is linked
+brew unlink docker-machine-driver-hyperkit && brew link docker-machine-driver-hyperkit
+
+# Caveats: This driver requires superuser privileges to access the hypervisor. To enable, execute
 sudo chown root:wheel $DRIVER_BINARY_PATH
 sudo chmod u+s $DRIVER_BINARY_PATH
 
